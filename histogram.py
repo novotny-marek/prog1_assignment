b8ee7d83f3e98c2cdf39b24e3fbdac8fe32864e0
novotny-marek <ahoj@mnovotny.xyz>
2024-02-04 15:54:12 +0100

    histogram.py Introduce ImageReader class

--- histogram.py
+++ histogram.py
@@ -1,3 +1,13 @@
 import imageio as iio
 import numpy as np
 import matplotlib.pyplot as plt
+
+class ImageReader:
+    def __init__(self, path):
+        self.image = iio.imread(path)
+    
+    def show_image(self):
+        plt.imshow(self.image)
+        plt.show()
+
+ImageReader('sample_image.jpg').show_image()
